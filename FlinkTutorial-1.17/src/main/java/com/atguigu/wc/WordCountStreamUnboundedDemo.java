1b37674f4f4b6c567bead5ac4b7c8851bdef7550
Forgethson_WMCT <649343453@qq.com>
2024-05-01 14:01:30 +0800

    代码提交：优化了WordCountBatchDemo、WordCountStreamDemo和WordCountStreamUnboundedDemo的代码结构，使其更加清晰易读。同时，修改了pom.xml中的依赖范围，以便在IDE中运行时能够正确加载。

--- FlinkTutorial-1.17/src/main/java/com/atguigu/wc/WordCountStreamUnboundedDemo.java
+++ FlinkTutorial-1.17/src/main/java/com/atguigu/wc/WordCountStreamUnboundedDemo.java
@@ -1,10 +1,7 @@
 package com.atguigu.wc;
 
-import org.apache.flink.api.common.typeinfo.TypeHint;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -18,20 +15,16 @@ import org.apache.flink.util.Collector;
  */
 public class WordCountStreamUnboundedDemo {
     public static void main(String[] args) throws Exception {
-        // TODO 1. 创建执行环境
+        // 1. 创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // IDEA运行时，也可以看到webui，一般用于本地测试
         // 需要引入一个依赖 flink-runtime-web
         // 在idea运行，不指定并行度，默认就是 电脑的 线程数
 //        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
-
-
         env.setParallelism(3);
-
-        // TODO 2. 读取数据： socket
-        DataStreamSource<String> socketDS = env.socketTextStream("hadoop102", 7777);
-
-        // TODO 3. 处理数据: 切换、转换、分组、聚合
+        // 2. 读取数据： socket
+        DataStreamSource<String> socketDS = env.socketTextStream("node1", 7777);
+        // 3. 处理数据: 切换、转换、分组、聚合
         SingleOutputStreamOperator<Tuple2<String, Integer>> sum = socketDS
                 .flatMap(
                         (String value, Collector<Tuple2<String, Integer>> out) -> {
@@ -43,16 +36,11 @@ public class WordCountStreamUnboundedDemo {
                 )
                 .setParallelism(2)
                 .returns(Types.TUPLE(Types.STRING,Types.INT))
-//                .returns(new TypeHint<Tuple2<String, Integer>>() {})
                 .keyBy(value -> value.f0)
                 .sum(1);
-
-
-
-        // TODO 4. 输出
+        // 4. 输出
         sum.print();
-
-        // TODO 5. 执行
+        // 5. 执行
         env.execute();
     }
 }
